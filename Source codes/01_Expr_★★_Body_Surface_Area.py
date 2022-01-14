43ad5dc245f2a24f0034ba50c44611be17bb99e2
Nukoumi139 <sitthapong2013@gmail.com>
2022-01-14 09:59:23 +0700

    Update 01_Expr_★★_Body_Surface_Area.py

--- Source codes/01_Expr_★★_Body_Surface_Area.py
+++ Source codes/01_Expr_★★_Body_Surface_Area.py
@@ -1,3 +1,20 @@
 import math as m
 
-print(m.log(100, 10))
+w = float(input())
+h = float(input())
+
+Hc_Const = 0.024265
+Hc_Wp = 0.5378
+Hc_Hp = 0.3964
+
+Boyd_Const = 0.0333
+Boyd_Wp = 0.6157 - (0.0188 * m.log10(w))
+Boyd_Hp = 0.3
+
+Mosteller_BSA = m.sqrt(w*h) / 60
+Haycock_BSA = Hc_Const * (w ** Hc_Wp) * (h ** Hc_Hp)
+Boyd_BSA = Boyd_Const * (w ** Boyd_Wp) * (h ** Boyd_Hp)
+
+print(Mosteller_BSA)
+print(Haycock_BSA)
+print(Boyd_BSA)
