d5e187ed139c55ac6d7d994faa7233ee2b26f759
Nukoumi139 <sitthapong2013@gmail.com>
2022-01-21 12:10:20 +0700

    Update (FINAL) 02_StrList_07_★★★_Decryption.py

--- Source codes/02 String and list/(FINAL) 02_StrList_07_★★★_Decryption.py
+++ Source codes/02 String and list/(FINAL) 02_StrList_07_★★★_Decryption.py
@@ -3,7 +3,9 @@ code = input()
 A = code[3] + code[10] + code[17] + code[24] + code[31]
 B = code[7] + code[12] + code[17] + code[22] + code[27]
 C = str(int(A) + int(B) + 10000)
+
 D = C[-4] + C[-3] + C[-2]
+
 E1 = int(D[0]) + int(D[1]) + int(D[2])
 E2 = (E1 % 10) + 1
 
