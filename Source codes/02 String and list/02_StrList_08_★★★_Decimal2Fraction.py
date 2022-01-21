bdebc3abcae9d5cb4b50fc1d2b1d73c215d74598
Nukoumi139 <sitthapong2013@gmail.com>
2022-01-21 09:13:35 +0700

    Update 02_StrList_08_★★★_Decimal2Fraction.py

--- Source codes/02 String and list/02_StrList_08_★★★_Decimal2Fraction.py
+++ Source codes/02 String and list/02_StrList_08_★★★_Decimal2Fraction.py
@@ -1,14 +1,5 @@
-from distutils.log import info
+dcm_num = input()
 
+comp = dcm_num.split(",")
 
-dcm = input()
-
-info = [0,0,0]
-comp = dcm.split(",")
 print(comp)
-
-info[0] = 0 + int(comp[0])
-info[1] = 0 + int(comp[1])
-info[2] = 0 + int(comp[2])
-
-print(empty)
