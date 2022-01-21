2bdae137b120bb167837eb1c0381fdf0867ae9e0
Nukoumi139 <sitthapong2013@gmail.com>
2022-01-21 12:20:19 +0700

    Finalized

--- Source codes/02 String and list/02_StrList_08_★★★_Decimal2Fraction.py
+++ Source codes/02 String and list/02_StrList_08_★★★_Decimal2Fraction.py
@@ -4,9 +4,13 @@ import math as ma
 num = input()
 comp = num.split(",")
 
+# keep in mind that all of these are STRING!!
 w = comp[0] # Whole number input
 si = comp[1] # Static decimal input, MIGHT BE AN EMPTY STRING
 r = comp[2] # Repeating decimal input
+
+
+
 
 ##### PROCESS : Troublesome processing zone #####
 
@@ -16,26 +20,23 @@ s = str(int(bool(si))) + si # bool(si) returns True (1) if exists, False (0) if 
 # if si ISN'T empty, the string (s) will be "1???" (added "1" on the leftmost side)
 # if si IS empty, the string (s) will be "0" (because si itself is empty)
 
-print("***Start of the program***\n")
-
 ## Analyzing the length and rearranging the data...
 
 n = len(s) # Starting length value (static decimal)
-s = str(int(s) % (10**(n-1))) # Fixing (originally) modified string d
+s = str(int(s) % (10**(n-1))) # Fixing (originally) modified string s
 # HOW...?
-# Since the string d is added leftmost "1" or "0", it must be removed here before further usages.
-# d is converted (from string) to an integer first, then "1" is removed by using modulus
+# Since the string s is added leftmost "1" or "0", it must be removed here before further usages.
+# s is converted (from string) to an integer first, then "1" is removed by using modulus
 # 10**(n-1) came from the concept that the position of removing (n)...
 # ...will be one value more than the power of 10 required to do modulus on the right position
-# ex. "125" -> len(d) = 3 = n -> ... % 100 == ... % (10**(2)) == ... % (10**(n-1))
+# ex. "125" -> len(s) = 3 = n -> ... % 100 == ... % (10**(2)) == ... % (10**(n-1))
 ## if the string is originally 0, this will return the same result
 
 n = n - 1 # Fixed length value -> the actual length after the data is being fixed (as "1" is removed, or sometimes not)
 # This also offset the member value when actually... the data arrived empty
 m = len(r) #Inputted repeating decimal length
 
-print("Static decimal length : " + str(n) + " (" + s + ")")
-print("Repeating decimal length : " + str(m) + " (" + r + ")")
+
 
 ### Finding true fraction for never ending, REPEATING DECIMAL places (r) ###
 
@@ -55,16 +56,12 @@ r_grcmd = ma.gcd(r_numerator, r_denominator) # finds greatest common divisor for
 r_simple_num = r_numerator // r_grcmd 
 r_simple_deno = r_denominator // r_grcmd
 
-print("\nFraction numerator for repeating decimal : " + str(r_numerator))
-print("Fraction denominator for repeating decimal : " + str(r_denominator))
-print("\nThe greatest common devisor for both of them : " + str(r_grcmd))
-print("\nSimplified repeating decimal numerator : " + str(r_simple_num))
-print("Simplified repeating decimal denominator : " + str(r_simple_deno))
+
 
 ### Finding true fraction for FIXED DECIMAL places (s) ###
 
 s_numerator = int(s) #normal decimal compared to unit
-s_denominator = 10**n #based on the location of the number to unit (actually.. the length of input)
+s_denominator = 10**n #based on the location of the number from unit place (actually.. the length of input)
 
 s_grcmd = ma.gcd(s_numerator, s_denominator) # finds greatest common divisor for the fraction
 
@@ -72,11 +69,6 @@ s_grcmd = ma.gcd(s_numerator, s_denominator) # finds greatest common divisor for
 s_simple_num = s_numerator // s_grcmd
 s_simple_deno = s_denominator // s_grcmd
 
-print("\nFraction numerator for static decimal : " + str(s_numerator))
-print("Fraction denominator for static decimal : " + str(s_denominator))
-print("\nThe greatest common devisor for both of them : " + str(s_grcmd))
-print("\nSimplified static decimal numerator : " + str(s_simple_num))
-print("Simplified static decimal denominator : " + str(s_simple_deno))
 
 
 ### Finding the COMBINED FRACTION (r + s) ###
@@ -84,21 +76,28 @@ print("Simplified static decimal denominator : " + str(s_simple_deno))
 # This is the way we add 2 fractions with different denominators
 dec_numerator = (s_simple_num * r_simple_deno) + (s_simple_deno * r_simple_num)
 dec_denominator = s_simple_deno * r_simple_deno
-# from... dn/
+# from... sn/sd + rn/rd = [(sn*rd) + (sd*rn)] / [sd*rd]
 
-dec_grcmd = ma.gcd(dec_numerator, dec_denominator)
+dec_grcmd = ma.gcd(dec_numerator, dec_denominator) # finds greatest common divisor for the fraction
 
+# Simplify
 dec_simple_num = dec_numerator // dec_grcmd
 dec_simple_deno = dec_denominator // dec_grcmd
 
-print("\nCombined decimal numerator : " + str(dec_numerator))
-print("Combined decimal denominator : " + str(dec_denominator))
-print("\nthe greatest common devisor for both of them : " + str(dec_grcmd))
-print("\nSimplified combined numerator : " + str(dec_simple_num))
-print("Simplified combined denominator : " + str(dec_simple_deno))
-
-### Finding the TRUE FRACTION OF THE DECIMAL ###
+## dec_simple_num will now be numerator for decimal part
+## dec_simple_deno will now be denominator for decimal part
 
 
+
+### FINALIZE : Finding the TRUE FRACTION OF THE ENTIRE DECIMAL ###
+
+# We have to combine the fraction from decimal part to the whole number given
+# let final_num be the finalized numerator
+# let final_deno be the finalized denominator
+
+final_num = dec_simple_num + (int(w) * dec_simple_deno)
+final_deno = dec_simple_deno
 
 ##### OUTPUT : Preparing appropriate outputs #####
+
+print(str(final_num) + " / " + str(final_deno))
