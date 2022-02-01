e3636e2a67ec29ec75e8ba3136530e7ad69c860e
Nukoumi139 <sitthapong2013@gmail.com>
2022-02-01 11:24:53 +0700

    Create Scratch paper 2.py

---
+++ Scratch works/Scratch paper 2.py
@@ -0,0 +1,19 @@
+from calendar import c
+
+
+
+def yay():
+    print("yayyy")
+
+def nah():
+    print("bruhh")
+
+cmd_list = {"y" : yay, "n" : nah}
+
+while True:
+    inp = str(input())
+    if inp == "exit":
+        break
+    elif inp in cmd_list:
+        cmd_list[inp]()
+
