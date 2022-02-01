9847067d5776574d5e8a2ac10d5c233e991ce220
Nukoumi139 <sitthapong2013@gmail.com>
2022-02-01 16:38:49 +0700

    Finished 03_If_01

--- Source codes/03 Selection/(FINAL) 03_If_01_★_FacultyCode.py
+++ Source codes/03 Selection/(FINAL) 03_If_01_★_FacultyCode.py
@@ -1,8 +1,6 @@
 inp = input()
 length = len(inp)
 
-print(length)
-
 char_list = 'abcdefghijklmnopqrstuvwxyz' * 2
 remain_code_list = ['01', '02', '51', '53', '55', '58']
 faculty_list = ['The Sirindhorn Thai Language Institute', 'General Education Center', 'Graduate School', 'School of Agricultural',
@@ -13,11 +11,20 @@ faculty_branches = ['Engineering', 'Arts', 'Science', 'Political Science', 'Arch
                     'Pharmaceutical Sciences', 'Law', 'Fine and Applied Arts', 'Nursing', 'Allied Health Sciences',
                     'Psychology', 'Sports Science']
 
-print(char_list)
-print(inp)
-
 if length == 2:
     if inp in char_list:
         print("Error")
     else:
-        x = int(inp)
+        if 20 <= int(inp) <= 40 or inp in remain_code_list:
+            print("OK")
+        else:
+            print("Error")
+
+elif length > 2:
+    if 'Faculty of' in inp and inp[11:] in faculty_branches:
+        print("OK")
+    else:
+        print("Error")
+
+else:
+    print("Error")
