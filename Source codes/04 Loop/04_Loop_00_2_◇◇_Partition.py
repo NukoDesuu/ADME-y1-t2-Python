3bf5e72986a6d3eb8ac29cf140f8e96dca67e0b5
Nukoumi139 <sitthapong2013@gmail.com>
2022-02-08 11:54:10 +0700

    Finalized 04_Loop_00_2

--- Source codes/04 Loop/04_Loop_00_2_◇◇_Partition.py
+++ Source codes/04 Loop/04_Loop_00_2_◇◇_Partition.py
@@ -1,1 +1,17 @@
-inp = [for i in input.split()]
+d = [int(i) for i in input().split()]
+
+n = len(d)
+p = d[-1]
+i = -1
+j = 0
+
+while j < (n - 1):
+    if d[j] <= p:
+        i += 1
+        d[i],d[j] = d[j],d[i]
+    
+    j += 1
+
+d[i+1],p = p,d[i+1]
+
+print(d)
