bc5a44b5ac48b2c14a43325084ae0ae1fd547a07
Nukoumi139 <sitthapong2013@gmail.com>
2022-02-08 17:35:03 +0700

    Finalized 04_Loop_11

---
+++ Source codes/04 Loop/(FINAL) 04_Loop_11_★★_RLE.py
@@ -0,0 +1,19 @@
+inp = input()
+
+ini = inp[0]
+out = ""
+n = 0
+
+for c in inp:
+    if c == ini:
+        n += 1
+    else:
+        out += (str(ini) + " " + str(n))
+        n = 0
+        ini = c
+        n += 1
+        out += " "
+
+out += (str(ini) + " " + str(n))
+
+print(out)
