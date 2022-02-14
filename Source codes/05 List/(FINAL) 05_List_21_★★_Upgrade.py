a792014765dc07c481cb494d71676095f8ad8c68
Nukoumi139 <sitthapong2013@gmail.com>
2022-02-14 21:43:33 +0700

    Finalized 05_List_21

---
+++ Source codes/05 List/(FINAL) 05_List_21_★★_Upgrade.py
@@ -0,0 +1,23 @@
+gradings = ['F', 'D', 'D+', 'C', 'C+', 'B', 'B+', 'A']
+
+ids = []
+grades = []
+
+inps = input()
+
+while inps != 'q':
+    ids.append(inps.split()[0])
+    grades.append(inps.split()[1])
+    inps = input()
+
+up_ids = input().split()
+
+for i in up_ids:
+    if i in ids:
+        who = ids.index(i)
+        g = gradings.index(grades[who])
+        g += 1
+        grades[who] = gradings[g]
+
+for n in range(len(ids)):
+    print(ids[n] + " " + grades[n])
