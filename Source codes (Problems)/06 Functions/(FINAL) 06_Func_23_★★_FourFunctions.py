fdb93ce66594a17535b59e534c81afee0c14e409
Nukoumi139 <sitthapong2013@gmail.com>
2022-02-17 23:00:34 +0700

    Finalized 06_Func_23

---
+++ Source codes (Problems)/06 Functions/(FINAL) 06_Func_23_★★_FourFunctions.py
@@ -0,0 +1,23 @@
+def make_int_list(x):
+    items = [int(n) for n in x.split()]
+    return items
+
+def is_odd(e):
+    if e % 2 == 1:
+        return True
+    return False
+
+def odd_list(alist):
+    o_list = []
+    for n in alist:
+        if is_odd(n) == True:
+            o_list.append(n)
+    return o_list
+
+def sum_square(alist):
+    sq_sum = 0
+    for n in alist:
+        sq_sum += (n**2)
+    return sq_sum
+
+print(sum_square([1,1,2,3]))
