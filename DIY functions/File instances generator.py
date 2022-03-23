40e40f0547a23012302ae1b2659130af09ea9f41
Nukoumi139 <sitthapong2013@gmail.com>
2022-03-23 17:09:21 +0700

    Create File instances generator.py

---
+++ DIY functions/File instances generator.py
@@ -0,0 +1,23 @@
+import codecs
+
+with codecs.open("input.txt", encoding='utf-8', mode='r') as f:
+    renamed = []
+    i = 0
+    for l in f:
+        if i == 0:
+            folder = l
+            i += 1
+        else:
+            items = l.split()
+            renamed.append(items[0] + ".py")
+
+dataPath = 'E:\\Nono\\Programming\\ONLINE SYNCED SOURCES\\ADME-y1-t2-Python\\Source codes (Problems)\\' + folder[:-2]
+
+print("Selected folder : " + folder[:-2])
+print("Path : " + dataPath)
+
+for e in renamed:
+    fullPath = dataPath + '\\' + e
+    open(fullPath, 'w').close()
+
+print('\ndone')
