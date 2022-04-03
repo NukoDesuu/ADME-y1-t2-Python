713ec7f2984027a82b3924ff1262e711b5beaf92
Nukoumi139 <sitthapong2013@gmail.com>
2022-04-03 23:00:56 +0700

    Update Scratch paper 8.py

---
+++ Scratch works/Scratch paper 8.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+t.pen(pencolor="red", fillcolor="red", pensize=3)
+t.begin_fill()
+t.left(50)
+t.forward(133)
+t.circle(50, 200)
+t.right(140)
+t.circle(50, 200)
+t.forward(133)
+t.end_fill()
+
+t.done()
