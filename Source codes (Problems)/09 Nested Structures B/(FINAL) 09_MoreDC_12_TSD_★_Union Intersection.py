caad31776c923fd32733c8717aaf1b4fdbadd914
Nukoumi139 <sitthapong2013@gmail.com>
2022-04-03 23:36:48 +0700

    Update (FINAL) 09_MoreDC_12_TSD_★_Union Intersection.py

--- Source codes (Problems)/09 Nested Structures B/(FINAL) 09_MoreDC_12_TSD_★_Union Intersection.py
+++ Source codes (Problems)/09 Nested Structures B/(FINAL) 09_MoreDC_12_TSD_★_Union Intersection.py
@@ -1,24 +1,27 @@
 n = int(input())
 
-# creating a list of sets
-i = 0
-sets = []
-while i < n:
-    new_set = set()
-    for e in input().split():
-        new_set.add(int(e))
-    sets.append(new_set)
-    i += 1
+# creating a list of sets.
+if n != 0:
+    i = 0
+    sets = []
+    while i < n:
+        new_set = set()
+        for e in input().split():
+            new_set.add(int(e))
+        sets.append(new_set)
+        i += 1
 
-print(sets)
+    # processes the list so we get unions and interceptions member counts.
+    first_set = sets[0]
+    unions_set = first_set
+    intersections_set = first_set
+    for cur_set in sets[1:]:
+        unions_set = unions_set.union(cur_set)
+        intersections_set = intersections_set.intersection(cur_set)
 
-# processes the list so we get unions and interceptions member counts
-first_set = sets[0]
-unions_set = first_set
-intersections_set = first_set
-for cur_set in sets[1:]:
-    unions_set = unions_set.union(cur_set)
-    intersections_set = intersections_set.intersection(cur_set)
-
-print(len(unions_set))
-print(len(intersections_set))
+    # prints out the number of member for each function.
+    print(len(unions_set))
+    print(len(intersections_set))
+else:
+    print(0)
+    print(0)
