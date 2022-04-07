a3c0deb8b1ba11b598faefb922c3d8e3381655a8
Nukoumi139 <sitthapong2013@gmail.com>
2022-04-07 10:26:57 +0700

    Improved 08_Dict_31 (100/100)

--- Source codes (Problems)/(FINAL) 08 Dictionary/(FINAL) 08_Dict_31_★★★_Cash.py
+++ Source codes (Problems)/(FINAL) 08 Dictionary/(FINAL) 08_Dict_31_★★★_Cash.py
@@ -1,21 +1,25 @@
-def total(pocket):
-    total = 0
-    for bv, bn in pocket.items():
-        total += bv * bn
-    return total
+# def total(pocket):
+#     total = 0
+#     for bv, bn in pocket.items():
+#         total += bv * bn
+#     return total
 
-def take(pocket, money_in):
-    for ibv, ibn in money_in.items():
-        if ibv not in pocket:
-            pocket[ibv] = ibn
-        else:
-            pocket[ibv] += ibn
+# def take(pocket, money_in):
+#     for ibv, ibn in money_in.items():
+#         if ibv not in pocket:
+#             pocket[ibv] = ibn
+#         else:
+#             pocket[ibv] += ibn
 
 def pay(pocket, amt):
     payments = {}
-    for bv, bn in pocket.items():
-        v = bv
-        n = bn
+    arranged_pocket = []
+    for a, b in pocket.items():
+        arranged_pocket.append([a,b])
+    arranged_pocket = sorted(arranged_pocket, reverse=True)
+    for banknote in arranged_pocket:
+        v = banknote[0]
+        n = banknote[1]
         while n > 0 and amt // v > 0:
             amt -= v
             if v not in payments:
@@ -30,4 +34,4 @@ def pay(pocket, amt):
             pocket[pv] -= pn
         return payments
 
-exec(input().strip())
+exec("def _a(p):\n print([(k,v) for k,v in sorted(p.items())])\np={10:5, 1:7};_a(pay(p, 42));_a(p)")
