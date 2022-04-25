c63b89a5b9a04e7f69f57dc8cecb1c1decf6220c
Nukoumi139 <sitthapong2013@gmail.com>
2022-04-26 01:14:01 +0700

    Finalized 10 Numpy 13

---
+++ Source codes (Problems)/10 Numpy/(FINAL) 10_NumPy_13_NumPy_★_Logistic_Regression.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def p(x):
+    return np.array([1 / (1 + np.e**-( -3.98 + (0.1 * x[i][0]) + (0.5 * x[i][1]) ) ) for i in range(x.shape[0])])
+
+print(p(np.array([[80, 2.50], [1, 4.00]])))
