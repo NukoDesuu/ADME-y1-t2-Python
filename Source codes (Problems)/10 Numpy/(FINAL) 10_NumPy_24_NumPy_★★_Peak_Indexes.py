25f399bdc2d3aac8a8b823489687e518ca1339a4
Nukoumi139 <sitthapong2013@gmail.com>
2022-04-27 19:59:34 +0700

    Finalized 10_Numpy_23

---
+++ Source codes (Problems)/10 Numpy/(FINAL) 10_NumPy_24_NumPy_★★_Peak_Indexes.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def peak_indexes(x):
+    num = x.shape[0]
+    pos = np.arange(num)
+    mid_pos = pos[1:num-1]
+    peak_locations = list()
+        
+    # head = x[0]
+    # mid = x[1:num]
+    # toe = x[num-1]
+        
+    # if head > mid[0]:
+    #     peak_locations.append([pos[0]])    
+    
+    peak_locations.append(list( mid_pos[ (x[mid_pos] > x[mid_pos + 1]) & (x[mid_pos] > x[mid_pos - 1]) ] ))
+
+    # if toe > mid[-2]:
+    #     peak_locations.append([pos[num-1]])
+
+    peak_locations = sum(peak_locations, [])
+
+    return peak_locations
+
+def main():
+    d = np.array([float(e) for e in input().split()])
+    pos = peak_indexes(np.array(d))
+    if len(pos) > 0:
+        print(", ".join([str(e) for e in pos]))
+    else:
+        print("No peaks")
+
+main()
